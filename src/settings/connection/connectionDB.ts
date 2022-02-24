5ab79cdeaae314a165e4282e069444e724270307
santiagoarias2000 <santiago.ariasm@usantoto.edu.co>
2022-02-24 14:05:34 -0500

    save

--- src/settings/connection/connectionDB.ts
+++ src/settings/connection/connectionDB.ts
@@ -1,17 +1,17 @@
 import pgPromise from "pg-promise";
-import {opcionesPG} from './optionConnection';
+import { opcionesPG } from './optionConnection';
 import var_conection from "../dominios/var_database";
 
-const pgp =pgPromise(opcionesPG);
+const pgp = pgPromise(opcionesPG);
 const pool = pgp(var_conection);
 
 pool.connect()
-.then((conn)=>{
-    console.log('TE CONECTASTES A: ', var_conection.database, 'EL USUARIO: ', var_conection.user);
-    conn.done();
-})
-.catch((meErr)=>{
-    console.log(meErr);
-});
+    .then((conn) => {
+        console.log('TE CONECTASTES A: ', var_conection.database, 'EL USUARIO: ', var_conection.user);
+        conn.done();
+    })
+    .catch((meErr) => {
+        console.log(meErr);
+    });
 
 export default pool;
